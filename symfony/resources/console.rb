deb3a5c38cc54a0bd15953bf6aff70aca84a38de
Florian Holzhauer <fh-github@fholzhauer.de>
2012-04-24 16:32:53 +0200

    bugfix, name_attribute is obviously stupid here

--- symfony/resources/console.rb
+++ symfony/resources/console.rb
@@ -1,6 +1,6 @@
 actions :cmd
 
-attribute :command, :kind_of => String, :name_attribute => true
+attribute :command, :kind_of => String
 attribute :path, :kind_of => String
 attribute :debug, :default => true
 attribute :env, :kind_of => String, :default => "dev"
