cfa818d3fe1b0521e594357693dd9659bbd9f136
Ole Michaelis <Ole.Michaelis@googlemail.com>
2012-05-30 16:33:41 +0200

    typo :D
    evil typo cookie monster

--- mongodb/metadata.rb
+++ mongodb/metadata.rb
@@ -1,7 +1,7 @@
 maintainer        "Digital Pioneers N.V."
 maintainer_email  "f.holzhauer@digitalpioneers.de"
 license           "Apache 2.0"
-description       "Installs and sets ups mongodb"
+description       "Installs and sets up mongodb"
 version           "0.1.0"
 recipe            "mongodb", "Installs mongodb"
 recipe            "mongodb::php", "Installs php driver for mongodb"
--- symfony/metadata.rb
+++ symfony/metadata.rb
@@ -1,7 +1,7 @@
 maintainer        "Digital Pioneers N.V."
 maintainer_email  "f.holzhauer@digitalpioneers.de"
 license           "Apache 2.0"
-description       "Installs and sets ups symfony"
+description       "Installs and sets up symfony"
 version           "0.1.0"
 recipe            "symfony", "Installs symfony"
 
