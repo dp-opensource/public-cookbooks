9b91a136a4446b60092ded0f504b090b16f5a930
Michael Kamphausen <mail@michael-kamphausen.de>
2012-06-28 12:45:51 +0200

    added cookbook for installation of all supported UTF-8 locales

---
+++ locale/metadata.rb
@@ -0,0 +1,8 @@
+maintainer        "Digital Pioneers N.V."
+maintainer_email  "m.kamphausen@digitalpioneers.de"
+license           "Apache 2.0"
+description       "Installs supported UTF-8 locales"
+version           "0.1.0"
+recipe            "locale", "Installs supported locales"
+
+supports "ubuntu"
