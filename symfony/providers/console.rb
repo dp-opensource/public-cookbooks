e724e9ed3cd045a4722de5d6ebc2c68650f4c66e
Ole Michaelis <Ole.Michaelis@googlemail.com>
2012-08-24 11:40:24 +0200

    Symfony recipe: console provider now use www-data as user

--- symfony/providers/console.rb
+++ symfony/providers/console.rb
@@ -2,12 +2,13 @@ action :cmd do
   execute "do console command" do
     user "www-data"
     cwd new_resource.path
-    
     Chef::Log.info("Executing console #{new_resource.command} in  #{new_resource.path}")
     if new_resource.debug
+      user "www-data"
       command "php app/console #{new_resource.command} --env=#{new_resource.env} " + new_resource.optional_params.join(' ')
     else 
-      command "php app/console #{new_resource.command} --env=#{new_resource.env} --no-debug " + new_resource.optional_params.join(' ')
+      user "www-data"
+      command "whoami && php app/console #{new_resource.command} --env=#{new_resource.env} --no-debug " + new_resource.optional_params.join(' ')
     end
   end
 end
